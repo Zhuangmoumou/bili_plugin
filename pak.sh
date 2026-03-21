bcb1b393801a87e21537a38f3fa61e79818d377d
Zhuangmoumou <zmm156300@163.com>
2026-03-21 17:18:51 +0800

    更新脚本

--- pak.sh
+++ pak.sh
@@ -3,10 +3,24 @@
 echo '编译Go服务器'
 cd go_server
 CGO_ENABLED=0 go build -ldflags="-s -w" -trimpath -o server
-echo '----------------'
-# 打包
 cd ..
-zip -r bili_plugin.zip go_server/server qml/ metadata.json build/linux/arm64-v8a/release/libbili_plugin.so FFmpegPlayer/ icon.png
+echo '----------------'
+
+# 临时文件
+mkdir bili_plugin
+cp go_server/server ./bili_plugin
+cp build/linux/arm64-v8a/release/libbili_plugin.so ./bili_plugin
+cp -r ./qml ./bili_plugin
+cp metadata.json ./bili_plugin
+cp icon.png ./bili_plugin
+cp -r FFmpegPlayer ./bili_plugin
+
+# 打包
+zip -r bili_plugin.zip bili_plugin/
+
+# 清除
+
+rm -r ./bili_plugin
 
 echo '----------------'
 echo '打包完成'
