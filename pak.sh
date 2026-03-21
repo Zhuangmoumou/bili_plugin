ac33c800a87af46b306ce6311b92b4508bb0dbfa
Zhuangmoumou <zmm156300@163.com>
2026-03-21 17:09:33 +0800

    更新脚本

--- pak.sh
+++ pak.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-# 编译Go服务器
+echo '编译Go服务器'
 cd go_server
 CGO_ENABLED=0 go build -ldflags="-s -w" -trimpath -o server
-
+echo '----------------'
 # 打包
 cd ..
 zip -r bili_plugin.zip go_server/server qml/ metadata.json build/linux/arm64-v8a/release/libbili_plugin.so FFmpegPlayer/ icon.png
 
+echo '----------------'
 echo '打包完成'
